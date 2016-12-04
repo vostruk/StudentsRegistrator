ac22e28ecc3d65c687c76ae125830275bdbf6829
Lukasz <lukier1@gmail.com>
2016-12-04 20:50:48 +0100

    Frontend template

---
+++ apsi_frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apsiFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the apsiFrontendApp
+ */
+angular.module('apsiFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
