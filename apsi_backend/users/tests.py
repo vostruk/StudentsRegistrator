ee2f66dd02052a1560909fd17b4b9e58cd7eec01
vovkaOst <vovaostruki@gmail.com>
2016-12-17 16:03:49 +0100

    correcting and adding some new test cases to the user app

--- apsi_backend/users/tests.py
+++ apsi_backend/users/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
+from django.contrib import auth
 from rest_framework import status
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import force_authenticate
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
 
 User = get_user_model()
@@ -11,26 +16,37 @@ class UsersEndpointTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.users_list_url = reverse('user-list')
+        cls.users_list_url = reverse('students-list')
+        cls.user = User.objects.create_user('testuser', email='testuser@test.com', type=2, password='testing')
 
     def test_users_list_url(self):
-        self.assertEqual(self.users_list_url, '/users/')
+        self.assertEqual(self.users_list_url, '/students/')
+        
+    def test_users_login(self):
+        self.client.login(username='lauren', password='secret')
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated())
+        self.client.login(username='testuser', password='testing')
+        user = auth.get_user(self.client)
+        self.assertTrue(user.is_authenticated())
 
     def test_create_student(self):
+        self.client.login(username='testuser', password='testing')
+        self.assertTrue(self.user.is_authenticated())
         post_data = {
-            'username': 'testuser',
+            'username': 'testuser2',
             'full_name': 'Jan Kowalski',
             'password': 'pass',
             'type': 0,
         }
-
-        response = self.client.post(self.users_list_url, post_data)
-
+        response = self.client.post(self.users_list_url, post_data, format='json')
+        print(response.content)
+        self.client.logout()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 1)
-        created_user = User.objects.first()
+        self.assertEqual(User.objects.count(),2)
+        created_user = User.objects.get(username="testuser2")
         self.assertTrue(created_user.is_student())
         self.assertTrue(created_user.check_password(post_data['password']))
         self.assertEqual(created_user.username, post_data['username'])
-        self.assertEqual(created_user.first_name, post_data['first_name'])
-        self.assertEqual(created_user.last_name, post_data['last_name'])
+        self.assertEqual(created_user.full_name, post_data['full_name'])
+        self.assertEqual(created_user.type, post_data['type'])
