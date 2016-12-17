46b84eab2e6c40ded905479ac4844976f70eaed8
marzuz <matmaz180@gmail.com>
2016-12-17 13:15:44 +0100

    walska z tockenem

--- apsi_frontend/app/scripts/controllers/courses.js
+++ apsi_frontend/app/scripts/controllers/courses.js
@@ -4,32 +4,42 @@
 'use strict';
 
 angular.module('apsiFrontendApp')
-	.controller('CoursesCtrl', function($scope) {
-		var c1= {
-        "code": "apsi",
-        "name": "Ap SI",
-        "syllabus": "bardzo fajny opis",
-        "tutor": 3,
-        "registered": null,
-        "state": 0
-    };
+	.controller('CoursesCtrl', function($http, $scope) {
 
-    var c2= {
-        "code": "MED",
-        "name": "ME Ddddd",
-        "syllabus": "asdasdasdasdasdasdasdas",
-        "tutor": 3,
-        "registered": null,
-        "state": 0
-    };
+		$http.get('http://localhost:8000/courses/',{
+      headers: {'Authorization': 'Token token=1887d746a393ab9193712d74b8ae37aacb14c15d'}
+    }).success(function (response) {
+        console.log(response);
+        $scope.records = response.data.records;
+      });
 
-    $scope.records = [c1,c2];
+
+
+    // var c1= {
+    //     "code": "apsi",
+    //     "name": "Ap SI",
+    //     "syllabus": "bardzo fajny opis",
+    //     "tutor": 3,
+    //     "registered": null,
+    //     "state": 0
+    // };
+    //
+    // var c2= {
+    //     "code": "MED",
+    //     "name": "ME Ddddd",
+    //     "syllabus": "asdasdasdasdasdasdasdas",
+    //     "tutor": 3,
+    //     "registered": null,
+    //     "state": 0
+    // };
+    //
+    // $scope.records = [c1,c2];
 
     $scope.detailClick = function (id) {
-      console.log(id+" redirect to detail")
+      console.log(id+' redirect to detail');
     };
 
     $scope.editClick = function (id) {
-      console.log(id+" redirect to detail")
+      console.log(id+' redirect to detail');
     };
 	});
