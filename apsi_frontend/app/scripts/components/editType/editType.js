50c8dabc54250c8a31669ea4dfc4fe162988a2a7
marzuz <matmaz180@gmail.com>
2016-12-26 19:46:53 +0100

    Dropbne poprawki obslugi terminow. nic istotnego

--- apsi_frontend/app/scripts/components/editType/editType.js
+++ apsi_frontend/app/scripts/components/editType/editType.js
@@ -6,14 +6,50 @@
 angular.module('apsiFrontendApp')
 	.controller('EditTypeCtrl', function($scope, $state, coursename, typeId, Restangular) {
     $scope.setVisibility = 'hidden';
+    $scope.week = [
+      {
+        id: 0,
+        name: 'Poniedzialek'
+      },
+      {
+        id: 1,
+        name: 'Wtorek'
+      },
+      {
+        id: 2,
+        name: 'Sroda'
+      },
+      {
+        id: 3,
+        name: 'Czwartek'
+      },
+      {
+        id: 4,
+        name: 'Piatek'
+      }
+    ];
 
+    function findDay(day_id) {
+        for (var i=0; i<$scope.week.length; i++) {
+          if($scope.week[i].id === day_id){
+            return $scope.week[i].name;
+          }
+        }
+    }
+
+    function dayFormat(termines) {
+        for (var i=0; i<termines.length; i++) {
+          termines[i].day = findDay(termines[i].day)
+        }
+        return termines;
+    }
     //TODO pobieranie terminow do typu i wyswietlenie listy
-    // Restangular.oneUrl('asd','http://url/').get()
-    //     .then( function(termines) {
-    //         console.log('Get termines:  ' + termines);
-    //         $scope.termines = termines;
-    //       }
-    //     );
+    Restangular.oneUrl('asd','http://localhost:8000/courses/'+coursename+'/class_types/'+typeId+'/time_slots/').get()
+        .then( function(termines) {
+            console.log('Get termines:  ' + termines);
+            $scope.termines = dayFormat(termines);
+          }
+        );
 
     Restangular.oneUrl('asd','http://localhost:8000/courses/'+coursename+'/class_types/'+typeId+'/').get()
         .then( function(type) {
@@ -40,28 +76,7 @@ angular.module('apsiFrontendApp')
         );
     };
 
-    $scope.week = [
-      {
-        id: 1,
-        name: 'Poniedzialek'
-      },
-      {
-        id: 2,
-        name: 'Wtorek'
-      },
-      {
-        id: 3,
-        name: 'Sroda'
-      },
-      {
-        id: 4,
-        name: 'Czwartek'
-      },
-      {
-        id: 5,
-        name: 'Piatek'
-      }
-    ];
+
     $scope.day = $scope.week[0];
 
     $scope.addTermine = function() {
@@ -70,31 +85,31 @@ angular.module('apsiFrontendApp')
 
     $scope.saveTermine = function() {
       var termine = {
-        day: $scope.day,
-        from: $scope.fromInput,
-        to: $scope.toInput
+        day: $scope.day.id,
+        time: $scope.fromInput
       };
       //TODO zapis terminu
-      // Restangular.oneUrl('asd','http://localhost:8000/courses/'+coursename).post('asd/',termine).then(
-      //     function() {
-      //     console.log('Created new termine.  ');
-      //     $scope.reload()
-      //     },
-      //     function()
-      //     {
-      //       console.log('Cannot create termine.');
-      //     }
-      //   )};
-      $scope.setVisibility = 'hidden';
+      Restangular.oneUrl('asd','http://localhost:8000/courses/'+coursename+'/class_types/'+typeId+'/').post('time_slots/',termine).then(
+          function() {
+            console.log('Created new termine.  ');
+            $scope.setVisibility = 'hidden';
+            $state.reload();
+          },
+          function()
+          {
+            console.log('Cannot create termine.');
+          }
+        );
+
 
     };
 
     $scope.removeTermine = function(id) {
       //TODO usuwanie terminu
-      // Restangular.oneUrl('courses', 'http://localhost:8000/url/').remove()
-      //   .then(function() {
-      //     console.log(id+' deleted');
-      //     $state.reload();
-      // });
+      Restangular.oneUrl('courses', 'http://localhost:8000/courses/'+coursename+'/class_types/'+typeId+'/time_slots/'+id+'/').remove()
+        .then(function() {
+          console.log(id+' deleted');
+          $state.reload();
+      });
     };
 	});
